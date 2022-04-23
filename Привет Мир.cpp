4606fdd561479f557371b8340163ca4e9989c698
Vlad <vald5red@yandex.ru>
2022-04-23 22:54:36 +0300

    The my first commit

---
+++ Привет Мир.cpp
@@ -0,0 +1,16 @@
+﻿// Привет Мир.cpp
+//
+
+#include <iostream>
+#include <locale>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");	//Подключили русский язык
+    std::cout << "Привет Мир!\n";
+	return 0;
+}
+
